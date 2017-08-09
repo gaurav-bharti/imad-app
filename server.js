80601eb0daabf10547e0ae88ecc0d628b5d4f81e
gaurav-bharti <30748339+gaurav-bharti@users.noreply.github.com>
2017-08-09 14:23:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -22,9 +22,7 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
-app.get('/favicon.ico', function (req, res) {
-  res.send("");
-});
+
 
 
 var articles = {
