cee02ce30ae627bb40baf62e1e14fe4bcf8c9e42
gaurav-bharti <30748339+gaurav-bharti@users.noreply.github.com>
2017-08-09 14:12:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -17,3 +17,26 @@ request.open('GET', 'http://gauravbharti4991.imad.hasura-app.io/counter', true);
 request.send(null);
 };
 
+var submitB = document.getElementById('submit');
+submitB.onClick = function() {
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+    var nameInput = documnet.getElementById('name');
+    var name = nameInput.value;
+	if(request.readyState === XMLHttpRequest.DONE) {
+		if(request.status === 200) {
+			var names = request.responseText;
+			names = JSON.parse(names);
+			var list = '';
+			for(var i=0; i<names.length; i++) {
+			    list += '<li>'+names[i]+'</li>';
+			}
+			var ul = document.getElementById('namelist');
+			ul.innerHTML = list;
+		}
+	}
+};
+
+request.open('GET', 'http://http://gauravbharti4991.imad.hasura-app.io/submit-name?name='+name, true);
+request.send(null);
+};
