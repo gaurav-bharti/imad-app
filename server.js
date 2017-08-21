47491f25ff9cfc0fb46f268528741a62fdf43291
gaurav-bharti <30748339+gaurav-bharti@users.noreply.github.com>
2017-08-21 19:04:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,7 +13,8 @@ var config = {
     host: 'db.imad.hasura-app.io',
     port: '5432',
     password: process.env.DB_PASSWORD
-}
+};
+
 app.get('/test-db', function (req, res) {
   pool.query('SELECT * FROM test', function(err, result) {
       if(err) {
