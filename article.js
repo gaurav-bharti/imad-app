b4a31d7badeb834b32b0bbb0643808dcf03dbcfa
gaurav-bharti <30748339+gaurav-bharti@users.noreply.github.com>
2017-08-24 16:27:08 +0530

    [imad-console] Updates article.js

--- article.js
+++ article.js
@@ -7,8 +7,8 @@ var bodyParser = require('body-parser');
 var session = require('express-session');
 
 var config = {
-    user: 'coco98',
-    database: 'coco98',
+    user: 'gauravbharti4991',
+    database: 'gauravbharti4991',
     host: 'db.imad.hasura-app.io',
     port: '5432',
     password: process.env.DB_PASSWORD
@@ -85,8 +85,7 @@ app.get('/hash/:input', function(req, res) {
 });
 
 app.post('/create-user', function (req, res) {
-   // username, password
-   // {"username": "tanmai", "password": "password"}
+    
    // JSON
    var username = req.body.username;
    var password = req.body.password;
@@ -232,7 +231,7 @@ app.get('/ui/:fileName', function (req, res) {
 });
 
 
-var port = 8080; // Use 8080 for local development because you might already have apache running on 80
-app.listen(8080, function () {
+var port = 80; // Use 8080 for local development because you might already have apache running on 80
+app.listen(port, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
